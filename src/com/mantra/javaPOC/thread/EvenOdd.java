666aaccdaa04b9e58c96928da618db1c443c904a
abhishek25cse@gmail.com <abhishek25cse@gmail.com>
2021-03-05 22:07:00 +0530

    even odd via 2 thread

--- src/com/mantra/javaPOC/thread/EvenOdd.java
+++ src/com/mantra/javaPOC/thread/EvenOdd.java
@@ -1,54 +1,62 @@
 package com.mantra.javaPOC.thread;
 
 public class EvenOdd {
-
-	public static volatile Integer count = 0;
-
-	public synchronized void printEven() throws InterruptedException {
-		System.out.println("Even " + count);
-		wait(100);
-	}
-
-	public synchronized void printOdd() throws InterruptedException {
-		System.out.println("Odd 	" + count);
-		notifyAll();
-	}
-	
-	public static synchronized void turnManager() throws InterruptedException {
-		EvenOdd eo = new EvenOdd();
-		synchronized (count) {
-			if (count % 2 == 0) {
-				try {
-					eo.printEven();
-					count++;
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			} else {
-				try {
-					eo.printOdd();
-					count++;
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	public static void main(String[] a) {		
-		Runnable r1 = () -> {
-			while (count < 20) {
-				try {
-					EvenOdd.turnManager();
-				} catch (InterruptedException e) {					
-					e.printStackTrace();
-				}
-			}
-		};
-		Thread odd = new Thread(r1);
-		Thread even = new Thread(r1);
+	public static void main(String[] a) {
+		OddEvenMoniter r1 = new OddEvenMoniter();
+		Thread odd = new Thread(r1, "Odd");
+		Thread even = new Thread(r1, "Even");
 		odd.start();
 		even.start();
+	}
+
+}
+
+class OddEvenMoniter implements Runnable {
+	public static  int count = 0;
+
+	@Override
+	public void run() {
+		while (count < 20) {
+			try {
+				turnManager();
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	public  void printEven() throws InterruptedException {
+		System.out.println(Thread.currentThread().getName()+"----" + count);	
+	}
+
+	public  void printOdd() throws InterruptedException {
+		System.out.println(Thread.currentThread().getName()+"		------" + count);		
+	}
+
+	public synchronized void turnManager() throws InterruptedException {
+		if (count % 2 == 0 && Thread.currentThread().getName().equals("Even")) {
+			try {
+				this.printEven();
+				count++;
+				wait(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+				
+		} else if(count % 2 == 1 && Thread.currentThread().getName().equals("Odd")) {
+			try {
+				this.printOdd();
+				count++;
+				notify();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		} else {
+			//System.out.println("SKIP**************");
+		}
 	}
 
 }
